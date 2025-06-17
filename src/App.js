aa941f152062ed590c3ec72110e4a3cd88956ad8
Your Name <158471535+JBD-Naman@users.noreply.github.com>
2025-06-17 20:55:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         // Fetch country data from API
         const fetchCountries = async () => {
             try {
-                const response = await fetch('https://restcountries.com/v3.1/all');
+                const response = await fetch('https://countries-search-data-prod-812920491762.asia-south1.run.app/countries');
                 if (!response.ok) throw new Error('Network response was not ok.');
                 const data = await response.json();
                 setCountries(data);
